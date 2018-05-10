1a051c438512c94a75b18884ae761a2505e2ea35
wystefan <910327256@qq.com>
2018-05-10 18:50:55 -0400

    added user admin controller, model, repository

---
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -0,0 +1,30 @@
+
+//IIFE immediately invoke
+(function () {
+    jQuery(main);
+
+    function main() {
+        var h1 = jQuery('#title');
+        h1.css('color', 'red');
+        h1.html('User Administration!');
+
+        var red = $('.red');
+        red.css('color', 'white').css('background-color', 'red');
+
+        var tr = $('.template');
+        var tbody = $('tbody');
+        var users = [{username: 'bob'}, {username: 'charlie'}];
+
+        for (var i = 0; i < users.length; i++) {
+            var user = users[i];
+            console.log(user);
+            var clone = tr.clone();
+            clone.find('.username').html(user.username);
+            tbody.append(clone);
+        }
+    }
+})();
+
+
+
+
