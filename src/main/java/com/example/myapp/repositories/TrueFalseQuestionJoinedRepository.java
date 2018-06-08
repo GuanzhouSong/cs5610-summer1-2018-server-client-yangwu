e6738f86598797931c14fce17d15c688b3e39bdf
wystefan <910327256@qq.com>
2018-06-07 21:43:05 -0400

    update

--- src/main/java/com/example/myapp/repositories/TrueFalseQuestionJoinedRepository.java
+++ src/main/java/com/example/myapp/repositories/TrueFalseQuestionJoinedRepository.java
@@ -1,12 +1,7 @@
 package com.example.myapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
-
 import com.example.myapp.models.TrueFalseQuestionJoined;
-
-
-
 
 public interface TrueFalseQuestionJoinedRepository extends CrudRepository<TrueFalseQuestionJoined, Integer> {
 
